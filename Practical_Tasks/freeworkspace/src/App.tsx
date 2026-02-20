b33c17704a0bfd8b87e549eb49f088804800e90f
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-20 19:07:40 +0500

    feat: добавлен Tailwind CSS и обновлен компонент App для улучшения стилей

--- Practical_Tasks/freeworkspace/src/App.tsx
+++ Practical_Tasks/freeworkspace/src/App.tsx
@@ -1,8 +1,12 @@
 export default function App() {
   return (
     <>
-      <h1>Привет, мир!</h1>
-      <p>Это мой первый React-приложение.</p>
+      <div className="align-center flex min-h-screen flex-col justify-center gap-4 text-center">
+        <h1 className="text-blue-600 dark:text-sky-400">Привет, мир!</h1>
+        <p className="text-red-400 dark:text-pink-400">
+          Это мой первый React-приложение.
+        </p>
+      </div>
     </>
-  )
+  );
 }
