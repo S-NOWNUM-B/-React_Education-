0b4508eaa592b00c232f74bc92632716f1950016
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-22 01:41:29 +0500

    fix: исправлены классы для выравнивания и размеров кнопки в компонентах App и Button

--- Practical_Tasks/freeworkspace/src/App.tsx
+++ Practical_Tasks/freeworkspace/src/App.tsx
@@ -3,7 +3,7 @@ import Button from "./components/Button/Button";
 export default function App() {
   return (
     <>
-      <div className="align-center flex min-h-screen flex-col justify-center gap-4 text-center">
+      <div className="flex min-h-screen flex-col justify-center items-center gap-4 text-center">
         <h1 className="text-blue-600 dark:text-sky-400">Привет, мир!</h1>
         <p className="text-red-400 dark:text-pink-400">
           Это мой первый React-приложение.
--- Practical_Tasks/freeworkspace/src/components/Button/Button.tsx
+++ Practical_Tasks/freeworkspace/src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 function Button() {
   return (
-    <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 w-32 h-20">
+    <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 w-40 h-10">
       Нажми меня
     </button>
   );
