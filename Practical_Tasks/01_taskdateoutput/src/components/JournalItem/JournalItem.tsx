892a3b6ac23933039f87d200d9969025904098ff
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-23 22:49:36 +0500

    feat: обновлены стили приложения с использованием Tailwind CSS и улучшено отображение компонентов

--- Practical_Tasks/01_taskdateoutput/src/components/JournalItem/JournalItem.tsx
+++ Practical_Tasks/01_taskdateoutput/src/components/JournalItem/JournalItem.tsx
@@ -8,7 +8,7 @@ function JournalItem({ title, text, date }: JournalItemProps) {
   return (
     <div className="rounded-[3px] bg-[#3a3a3a] text-white p-5">
       <h2 className="text-lg font-semibold leading-7">{title}</h2>
-      <div className="flex gap-[10px]">
+      <div className="flex gap-2.5">
         <div className="text-white/40 text-base font-normal leading-normal">
           {date.toString()}
         </div>
--- Practical_Tasks/freeworkspace/src/components/Button/Button.tsx
+++ Practical_Tasks/freeworkspace/src/components/Button/Button.tsx
@@ -1,7 +1,9 @@
-import "./Button.css";
-
 function Button() {
-  return <button className="button-save button-accent">Сохранить</button>;
+  return (
+    <button className="text-base px-8 py-2 font-sans cursor-pointer transition-colors duration-250 bg-[#312eb5] hover:bg-[#222096] text-white rounded">
+      Сохранить
+    </button>
+  );
 }
 
 export default Button;
