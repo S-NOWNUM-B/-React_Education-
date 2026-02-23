ad6504034334652d878fb68a11d9b46b5a854c0d
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-23 15:07:00 +0500

    feat: добавлен массив данных для компонента SkillsItems и обновлён компонент App для его отображения; рефакторинг компонента Button

--- Practical_Tasks/freeworkspace/src/App.tsx
+++ Practical_Tasks/freeworkspace/src/App.tsx
@@ -3,6 +3,24 @@ import "./components/SkillsItems/SkillsItems";
 import SkiilsItems from "./components/SkillsItems/SkillsItems";
 
 export default function App() {
+  const data = [
+    {
+      title: "Подготовка к собеседованию",
+      text: "Создайте компонент SkillsItems, который отображает список навыков. Компонент должен принимать массив навыков в виде пропса и отображать их в виде списка. Также добавьте заголовок для списка навыков и текущую дату.",
+      date: new Date(),
+    },
+    {
+      title: "Второй элемент",
+      text: "Второй элемент данных для отображения в компоненте SkillsItems.",
+      date: new Date(),
+    },
+    {
+      title: "Третий элемент",
+      text: "Третий элемент данных для отображения в компоненте SkillsItems.",
+      date: new Date(),
+    }
+  ]
+
   return (
     <>
       <div className="flex min-h-screen flex-col justify-center items-center gap-4 text-center bg-gray-700">
@@ -11,7 +29,16 @@ export default function App() {
           Это мой первый React-приложение.
         </p>
         <Button />
-        <SkiilsItems />
+        <SkiilsItems
+          title={data[0].title}
+          text={data[0].text}
+          date={data[0].date} 
+        />
+        <SkiilsItems
+          title={data[1].title}
+          text={data[1].text}
+          date={data[1].date}
+         />
       </div>
     </>
   );
--- Practical_Tasks/freeworkspace/src/components/SkillsItems/SkillsItems.tsx
+++ Practical_Tasks/freeworkspace/src/components/SkillsItems/SkillsItems.tsx
@@ -1,36 +1,25 @@
-const SkillsInfo = {
-  title: "Подготовка к собеседованию",
-  skills: ["HTML", "CSS", "JavaScript", "React", "TypeScript"],
-  data: new Date(),
-  text: "Создайте компонент SkillsItems, который отображает список навыков. Компонент должен принимать массив навыков в виде пропса и отображать их в виде списка. Также добавьте заголовок для списка навыков и текущую дату.",
-};
+const skills = ["JavaScript", "TypeScript", "React", "Node.js", "CSS"];
 
-function SkillsItems() {
+interface SkillsItemsProps {
+  title: string;
+  text: string;
+  date: Date;
+}
+
+function SkillsItems({ title, text, date }: SkillsItemsProps) {
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-4 text-center bg-blue-300 p-4 rounded w-100 h-150">
-        <h2 className="text-4xl font-bold bg-amber-600">{SkillsInfo.title}</h2>
+        <h2 className="text-4xl font-bold bg-amber-600">{title}</h2>
         <div className="flex flex-col gap-2">
-          <div className="text-2xl font-light bg-emerald-400">
-            {SkillsInfo.skills[0]}
-          </div>
-          <div className="text-2xl font-light bg-emerald-400">
-            {SkillsInfo.skills[1]}
-          </div>
-          <div className="text-2xl font-light bg-emerald-400">
-            {SkillsInfo.skills[2]}
-          </div>
-          <div className="text-2xl font-light bg-emerald-400">
-            {SkillsInfo.skills[3]}
-          </div>
-          <div className="text-2xl font-light bg-emerald-400">
-            {SkillsInfo.skills[4]}
-          </div>
+          <div className="text-2xl font-light bg-emerald-400">{skills[0]}</div>
+          <div className="text-2xl font-light bg-emerald-400">{skills[1]}</div>
+          <div className="text-2xl font-light bg-emerald-400">{skills[2]}</div>
+          <div className="text-2xl font-light bg-emerald-400">{skills[3]}</div>
+          <div className="text-2xl font-light bg-emerald-400">{skills[4]}</div>
         </div>
-        <p className="text-lg bg-cyan-400">{SkillsInfo.text}</p>
-        <p className="text-lg bg-cyan-400">
-          {SkillsInfo.data.toLocaleDateString()}
-        </p>
+        <p className="text-lg bg-cyan-400">{text}</p>
+        <p className="text-lg bg-cyan-400">{date.toLocaleDateString()}</p>
       </div>
     </>
   );
