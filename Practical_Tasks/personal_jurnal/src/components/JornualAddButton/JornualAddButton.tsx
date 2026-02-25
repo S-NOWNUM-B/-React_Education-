2a32e4f863fc63a13f246f66d6f3bcf603bbe6b7
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-25 22:34:22 +0500

    feat: добавлены компоненты JornualAddButton, JornualList, Body и LeftPanel

---
+++ Practical_Tasks/personal_jurnal/src/components/JornualAddButton/JornualAddButton.tsx
@@ -0,0 +1,12 @@
+import "../CardButton/CardButton";
+import CardButton from "../CardButton/CardButton";
+
+import "./JornualAddButton.css";
+
+function JornualAddButton() {
+  return <CardButton>
+	Новое воспоминание
+  </CardButton>;
+}
+
+export default JornualAddButton;
---
+++ Practical_Tasks/personal_jurnal/src/components/JornualList/JornualList.tsx
@@ -0,0 +1,11 @@
+import "./JornualList.css";
+
+function JornualList({ children }) {
+  return (
+	<div className="journal-list">
+		{children}
+	</div>
+  );
+}
+
+export default JornualList;
