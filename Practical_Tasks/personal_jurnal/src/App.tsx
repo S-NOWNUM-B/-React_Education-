60b5a73252ed02142b10e4b6fcc5d8b25851d69c
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-02 19:22:12 +0500

    feat: добавлен функционал для добавления элементов в журнал и оптимизирован компонент JournalForm

--- Practical_Tasks/personal_jurnal/src/App.tsx
+++ Practical_Tasks/personal_jurnal/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import LeftPanel from "./layouts/LeftPanel/LeftPanel";
 import Body from "./layouts/Body/Body";
@@ -9,19 +11,38 @@ import JournalItem from "./components/JournalItem/JournalItem";
 import JornualList from "./components/JornualList/JornualList";
 import JournalForm from "./components/JournalForm/JournalForm";
 
+interface JournalItem {
+  title: string;
+  text: string;
+  date: Date;
+}
+
+const INITIAL_DATA = [
+  {
+    title: "Подготовка к обновлению курсов",
+    text: "Горные походы открывают удивительные природные ландшафт",
+    date: new Date("2023-07-18T17:23:14+03:00"),
+  },
+  {
+    title: "Поход в годы",
+    text: "Думал, что очень много времени",
+    date: new Date("2023-07-18T17:23:14+03:00"),
+  },
+];
+
 function App() {
-  const data = [
-    {
-      title: "Подготовка к обновлению курсов",
-      text: "Горные походы открывают удивительные природные ландшафт",
-      date: new Date("2023-07-18T17:23:14+03:00"),
-    },
-    {
-      title: "Поход в годы",
-      text: "Думал, что очень много времени",
-      date: new Date("2023-07-18T17:23:14+03:00"),
-    },
-  ];
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = (item: JournalItem) => {
+    setItems((oldItems) => [
+      ...oldItems,
+      {
+        text: item.text,
+        title: item.title,
+        date: new Date(item.date),
+      },
+    ]);
+  };
 
   return (
     <div className="app">
@@ -29,19 +50,15 @@ function App() {
         <Header />
         <JournalAddButton />
         <JornualList>
-          {data.map(el => (
+          {items.map((el) => (
             <CardButton key={el.title}>
-              <JournalItem
-                title={el.title}
-                text={el.text}
-                date={el.date}
-            />
-          </CardButton>
+              <JournalItem title={el.title} text={el.text} date={el.date} />
+            </CardButton>
           ))}
         </JornualList>
       </LeftPanel>
       <Body>
-        <JournalForm />
+        <JournalForm onSubmit={addItem} />
       </Body>
     </div>
   );
--- Practical_Tasks/personal_jurnal/src/components/JournalForm/JournalForm.tsx
+++ Practical_Tasks/personal_jurnal/src/components/JournalForm/JournalForm.tsx
@@ -1,34 +1,22 @@
 import "./JournalForm.css";
 
-import { useState } from "react";
 import Button from "../Button/Button.tsx";
 
-function JournalForm() {
-  const [inputData, setInputData] = useState("");
-
-  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputData(event.target.value);
-  };
-
-  const addJournalItem = (e: React.FormEvent<HTMLFormElement>) => {
+function JournalForm({ onSubmit }) {
+  const addJournalItem = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
-    const FormProps = Object.fromEntries(formData);
-    console.log(FormProps);
-  };
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData.entries());
+    onSubmit(formProps);
+  }
 
   return (
     <>
       <form className="journal-form" onSubmit={addJournalItem}>
         <input type="text" name="title" />
         <input type="date" name="date" />
-        <input
-          type="text"
-          name="tag"
-          value={inputData}
-          onChange={inputChange}
-        />
-        <textarea name="post" cols={30} rows={10} />
+        <input type="text" name="tag" />
+        <textarea name="text" id="" cols={30} rows={10} />
         <Button text="Сохранить" onClick={() => console.log("Нажали")} />
       </form>
     </>
