421563b911ebb281d2ef00046a45c335a0bb8281
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-09 12:54:02 +0500

    feat: добавлен выпадающий список для выбора пользователя в компонент Header

--- Practical_Tasks/personal_jurnal/src/components/Header/Header.tsx
+++ Practical_Tasks/personal_jurnal/src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import styles from "./Header.module.css";
 
 function Header() {
-  return <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />;
+  return (
+    <>
+      <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+      <select name="user" id="user">
+        
+      </select>
+    </>
+  );
 }
 
 export default Header;
