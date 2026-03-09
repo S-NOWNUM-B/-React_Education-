eb6198cdc79ee5a3272736bba2c67cc9e0f83050
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-09 13:23:54 +0500

    feat: добавлен обработчик изменения пользователя в компонент Header

--- Practical_Tasks/personal_jurnal/src/components/Header/Header.tsx
+++ Practical_Tasks/personal_jurnal/src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import styles from "./Header.module.css";
 
-function Header() {
+function Header({ changedUser}) {
+  const changeUser = (e: { target: { value: unknown; }; }) => {
+    changedUser(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
-      <select name="user" id="user">
-        
+      <select name="user" id="user" onChange={changeUser}>
+        <option value="ser1">Стас</option>
+        <option value="ser2">Петя</option>
+        <option value="ser3">Вася</option>
       </select>
     </>
   );
