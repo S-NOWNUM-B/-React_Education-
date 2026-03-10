72b689bdfd9ea4e34b653857d9532321941892ea
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-10 18:41:51 +0500

    feat: добавлены пропсы setItem и currentUserId в компонент JournalList, обновлена логика фильтрации записей

--- Practical_Tasks/personal_jurnal/src/components/JournalList/JournalList.tsx
+++ Practical_Tasks/personal_jurnal/src/components/JournalList/JournalList.tsx
@@ -5,13 +5,11 @@ import type { JournalItem as JournalItemType } from "../../types";
 
 interface JournalListProps {
   items: JournalItemType[];
+  setItem: (item: JournalItemType) => void;
+  currentUserId: number;
 }
 
-function JournalList({ items }: JournalListProps) {
-  if (items.length === 0) {
-    return <p>Записей пока нет, добавьте первую</p>;
-  }
-
+function JournalList({ items, setItem, currentUserId }: JournalListProps) {
   const sortItems = (a: JournalItemType, b: JournalItemType) => {
     if (a.date < b.date) {
       return 1;
@@ -20,10 +18,18 @@ function JournalList({ items }: JournalListProps) {
     }
   };
 
+  const filteredItems = items
+    .filter((el) => el.userId === currentUserId)
+    .sort(sortItems);
+
+  if (filteredItems.length === 0) {
+    return <p>Записей пока нет, добавьте первую</p>;
+  }
+
   return (
     <>
-      {[...items].sort(sortItems).map((el) => (
-        <CardButton key={el.id}>
+      {filteredItems.map((el) => (
+        <CardButton key={el.id} onClick={() => setItem(el)}>
           <JournalItem title={el.title} post={el.post} date={el.date} />
         </CardButton>
       ))}
